ae83a613074bdcb21a580bb17fdac1b99289296a
Masum Billah <fabrika786@gmail.com>
2024-02-04 01:49:07 +0600

    redux install

---
+++ redux/reducer/Reducers.js
@@ -0,0 +1,12 @@
+import { ADD_ITEM, REMOVE_ITEM } from "../ActionTypes";
+
+export const Reducers = (state = [], action) => {
+    switch (action.type) {
+        case ADD_ITEM: {
+
+        }
+        case REMOVE_ITEM: {
+
+        }
+    }
+}
